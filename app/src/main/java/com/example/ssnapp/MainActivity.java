1499e6a14730671efc5dd35f1caf5f39150ffcd9
Deepthi <deepthi17034@cse.ssn.edu.in>
2020-01-08 19:15:53 +0530

    register

--- app/src/main/java/com/example/ssnapp/MainActivity.java
+++ app/src/main/java/com/example/ssnapp/MainActivity.java
@@ -2,13 +2,98 @@ package com.example.ssnapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Calendar;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button selectDate;
+    TextView date;
+    DatePickerDialog datePickerDialog;
+    int year;
+    int month;
+    int dayOfMonth;
+    Calendar calendar;
+    Button reset;
+
+    EditText TF;
+    public Button checkButton;
+    Spinner myspinner;
+
+    EditText pass;
+    EditText repass;
+
+    public final Pattern EMAIL_ADDRESS_PATTERN = Pattern.compile(
+            "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+\\.+[a-z]+\\.+[a-z]+"
+    );
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        selectDate = findViewById(R.id.button);
+        date = findViewById(R.id.dob);
+        selectDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                calendar = Calendar.getInstance();
+                year = calendar.get(Calendar.YEAR);
+                month = calendar.get(Calendar.MONTH);
+                dayOfMonth = calendar.get(Calendar.DAY_OF_MONTH);
+                datePickerDialog = new DatePickerDialog(MainActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                                date.setText(day + "/" + (month + 1) + "/" + year);
+                            }
+                        }, year, month, dayOfMonth);
+                datePickerDialog.getDatePicker().setMaxDate(System.currentTimeMillis());
+                datePickerDialog.show();
+            }
+        });
+
+        myspinner = (Spinner) findViewById(R.id.spinner);
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(MainActivity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.Department));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        myspinner.setAdapter(myAdapter);
+
+
+        TF=(EditText) findViewById(R.id.email);
+        checkButton=(Button) findViewById(R.id.button2);
+        pass=findViewById(R.id.editText);
+        repass=findViewById(R.id.editText2);
+
+        checkButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String email=TF.getText().toString();
+                String password=pass.getText().toString();
+                String reenter=repass.getText().toString();
+                if(!checkEmail(email))
+                    Toast.makeText(MainActivity.this,"Invalid Email Addresss", Toast.LENGTH_SHORT).show();
+                else if(!(password.equals(reenter)))
+                    Toast.makeText(MainActivity.this,"Passwords do not match", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+    private boolean checkEmail(String email) {
+        return EMAIL_ADDRESS_PATTERN.matcher(email).matches();
     }
 }
+
